1a8be58abe52680a370a31edac727ccd7216dcf5
nhbthach <nhbthach@gmail.com>
2024-01-11 00:14:05 +0700

    check

--- src/containers/HomePage/ImgList.js
+++ src/containers/HomePage/ImgList.js
@@ -2,7 +2,7 @@ export let imgHomeHeader = {
   logo_logo:
     "https://raw.githubusercontent.com/thachnhb/medicbooking_frontend/master/src/assets/images/1_header/logo.svg",
   logo_text:
-    "https://raw.githubusercontent.com/thachnhb/medicbooking_frontend/master/src/assets/images/1_header/logo_text.png",
+    "https://raw.githubusercontent.com/thachnhb/medicbooking_frontend/master/src/assets/images/1_header/logoMedic.svg",
   google_play:
     "https://raw.githubusercontent.com/thachnhb/medicbooking_frontend/master/src/assets/images/2_banner/google_play.svg",
   app_store:
